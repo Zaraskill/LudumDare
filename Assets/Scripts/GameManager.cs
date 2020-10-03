0a52e06eb007bc996ca66f571036837f8058ab1a
6meon <malaretsimeon@gmail.com>
2020-10-03 11:16:25 +0200

    Push with new scene + Dash timer into GameManager (Singletoned)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    public float dashTime;
+    [SerializeField] private float _dashTimeLeft;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
+    }
+
+    public void Init()
+    {
+        _dashTimeLeft = dashTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (DashTimer())
+        {
+            ResetDashTimer();
+        }
+    }
+
+    private bool DashTimer()
+    {
+        _dashTimeLeft -= Time.deltaTime;
+
+        if (_dashTimeLeft <= 0)
+        {
+            Debug.Log("Le J c'est le S");
+            return true;
+        }
+        return false;
+    }
+
+    private void ResetDashTimer()
+    {
+        Debug.Log("J'sors le RS");
+        _dashTimeLeft = dashTime;
     }
 }
